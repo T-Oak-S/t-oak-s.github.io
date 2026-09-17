f0785f1ab4b5729474a8735cf3e65c8f8d81732d
Oak <tzs20080112@gmail.com>
2026-09-17 19:10:19 +0800

    Deploy site

---
+++ assets.js
@@ -0,0 +1,101 @@
+window.BADGE_ASSETS = {
+  "seu": {
+    "rect": [
+      0,
+      0,
+      578,
+      578
+    ],
+    "src": "assets/seu.png"
+  },
+  "nuist": {
+    "rect": [
+      149,
+      143,
+      1200,
+      1200
+    ],
+    "src": "assets/nuist.jpg"
+  },
+  "njupt": {
+    "rect": [
+      0,
+      0,
+      318,
+      345
+    ],
+    "src": "assets/njupt.png"
+  },
+  "njtech": {
+    "rect": [
+      393,
+      291,
+      252,
+      300
+    ],
+    "src": "assets/njtech.png"
+  },
+  "njnu": {
+    "rect": [
+      2,
+      3,
+      53,
+      54
+    ],
+    "src": "assets/njnu.png"
+  },
+  "njfu": {
+    "rect": [
+      190,
+      608,
+      380,
+      380
+    ],
+    "src": "assets/njfu.jpg"
+  },
+  "nju": {
+    "rect": [
+      0,
+      0,
+      1723,
+      2160
+    ],
+    "src": "assets/nju.jpg"
+  },
+  "njau": {
+    "rect": [
+      0,
+      0,
+      934,
+      934
+    ],
+    "src": "assets/njau.png"
+  },
+  "hhu": {
+    "rect": [
+      0,
+      0,
+      200,
+      200
+    ],
+    "src": "assets/hhu.jpg"
+  },
+  "nuaa": {
+    "rect": [
+      1745,
+      1074,
+      1100,
+      1100
+    ],
+    "src": "assets/nuaa.jpg"
+  },
+  "njust": {
+    "rect": [
+      1980,
+      528,
+      810,
+      810
+    ],
+    "src": "assets/njust.png"
+  }
+};
